63edcb40767d1a9375306cbea52e68d52775d5a3
nasthosh <santhosh.karthik@outlook.com>
2020-10-05 12:06:43 +1100

    Navigation DOne

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div className="courseapp">
+            <h1>React App</h1>
+            <p> Trying to do a new react app</p>
+        </div>
+    );
+}
+export default HomePage;
